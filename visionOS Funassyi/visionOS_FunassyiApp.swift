b9d51266ac3c6aa7390d6a3101336bb284d8d2f6
TETRA2000 <sagittariusm25@gmail.com>
2023-06-25 20:20:54 +0900

    Initial Commit

---
+++ visionOS Funassyi/visionOS_FunassyiApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionOS_FunassyiApp.swift
+//  visionOS Funassyi
+//
+//  Created by Takahiko Inayama on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct visionOS_FunassyiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
